18504ef04965ddf1e729a85a8847d8f7ec38f3f5
LucasFerreiraPrazeres <luucasfp@gmail.com>
2021-11-03 09:19:11 -0300

    Removendo comentário no main.js

--- main.js
+++ main.js
@@ -7,19 +7,6 @@ const linksSocialMedia = {
   twitter: 'LucasFe30258387'
 }
 
-/*primeira opção para alteração de conteúdo
-
-function changeSocialMediaLinks() {
-  document.getElementById('userName').textContent = 'Lucas'
-}
-*/
-
-/* segunda opção
-function changeSocialMediaLinks() {
-  userName.textContent = 'Lucas'
-}
-*/
-
 function changeSocialMediaLinks() {
   for (let li of socialLinks.children) {
     const social = li.getAttribute('class')
